7ecc3ccc0bebd68b563555f62e7a90217d4f0d7e
Jan Ouwens <jan.ouwens@ordina.nl>
2016-02-17 14:20:13 +0100

    Refactor to use the service to do the http call

--- src/header/avatar/avatar.ts
+++ src/header/avatar/avatar.ts
@@ -3,12 +3,12 @@ import {Http, Response} from 'angular2/http';
 
 @Injectable()
 class Service {
-  constructor() {
+  constructor(private http: Http) {
 
   }
   
   getAvatar() {
-    return '[Avatar]';   
+    return this.http.get('/data.json');
   }
 }
 
@@ -26,14 +26,12 @@ class Service {
 export class MyAvatar {
   avatar = '';
 
-  constructor(
-    private service: Service,
-    private http: Http) {
+  constructor(private service: Service) {
   }
 
   onClick() {
     var url = '/data.json';
-    this.http.get(url).subscribe((res: Response) => {
+    this.service.getAvatar().subscribe((res: Response) => {
       var json = res.json();
       this.avatar = json.data;
     });
