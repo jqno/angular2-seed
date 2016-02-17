2694ddb27ff6885836ec1c7d443cc25625cd4e98
Jan Ouwens <jan.ouwens@ordina.nl>
2016-02-17 11:59:40 +0100

    Add a Header directive

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,20 @@ import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
 
 @Component({
+  selector: 'header',
+  template: `
+  Header from component
+  `
+})
+class Header {
+
+}
+
+@Component({
   selector: 'seed-app',
+  directives: [
+    Header
+  ],
   template: `
   <div>
     <header>header</header>
