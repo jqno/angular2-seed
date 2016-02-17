4953aab3c142553a773969a0398e8e3326a2d701
Jan Ouwens <jan.ouwens@ordina.nl>
2016-02-17 12:31:44 +0100

    Add an avatar inside the header

---
+++ src/header/avatar/avatar.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-avatar',
+  template: `
+  [Avatar]
+  `
+})
+export class MyAvatar {
+
+}
+
--- src/header/header.ts
+++ src/header/header.ts
@@ -1,9 +1,14 @@
 import {Component} from 'angular2/core';
+import {MyAvatar} from './avatar/avatar';
 
 @Component({
   selector: 'my-header',
+  directives: [
+    MyAvatar
+  ],
   template: `
   Header from component
+  <my-avatar></my-avatar>
   `
 })
 export class MyHeader {
